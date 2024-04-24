cfd92d474c53c91aee608bc355ab2be0a03ef185
Laurent VANDELLE <vandellelaurent@yahoo.fr>
2024-04-24 19:29:45 +0200

    use bun instead of npm + fix ts build

--- src/api/todo/todo.contoller.ts
+++ src/api/todo/todo.contoller.ts
@@ -2,6 +2,7 @@ import { logger } from '@src/helpers/logger';
 import { HttpError } from '@src/models/global/http.model';
 
 export async function createTodo({ body }: { body: { name: string } }): Promise<any> {
+  // Just to test error middleware
   if (Object.keys(body).length === 0) {
     // throw new Error('BODY_NOT_PROVIDED_ERROR');
     throw new HttpError(400, 'BODY_NOT_PROVIDED_ERROR');
