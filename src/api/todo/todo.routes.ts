116360676934aed7fd3ccd7023225fce8896d6f2
TheSmartMonkey <vandellelaurent@yahoo.fr>
2025-12-02 16:57:33 +0100

    add a prettier rule to keep one line per routes in route files

--- src/api/todo/todo.routes.ts
+++ src/api/todo/todo.routes.ts
@@ -5,26 +5,14 @@ import { createTodoInput, createTodoOutput } from './schemas/create-todo.schema'
 import { deleteTodoInput, deleteTodoOutput } from './schemas/delete-todo.schema';
 import { getTodoByIdInput, getTodoByIdOutput } from './schemas/get-todo-by-id.schema';
 import { updateTodoInput, updateTodoOutput } from './schemas/update-todo.schema';
-import {
-  createTodoController,
-  deleteTodoController,
-  getAllUserTodosController,
-  getTodoByIdController,
-  updateTodoController,
-} from './todo.contoller';
+import { createTodoController, deleteTodoController, getAllUserTodosController, getTodoByIdController, updateTodoController } from './todo.contoller';
 
 const todo = new OpenAPIHono();
 
 todo.openapi(route.get('/user', 'todo'), controller(getAllUserTodosController));
-todo.openapi(
-  route.get('/todoId/{todoId}', 'todo', { input: getTodoByIdInput, output: getTodoByIdOutput }),
-  controller(getTodoByIdController),
-);
+todo.openapi(route.get('/todoId/{todoId}', 'todo', { input: getTodoByIdInput, output: getTodoByIdOutput }), controller(getTodoByIdController));
 todo.openapi(route.post('/', 'todo', { input: createTodoInput, output: createTodoOutput }), controller(createTodoController));
-todo.openapi(
-  route.delete('/todoId/{todoId}', 'todo', { input: deleteTodoInput, output: deleteTodoOutput }),
-  controller(deleteTodoController, { useCustomOutput: true }),
-);
+todo.openapi(route.delete('/todoId/{todoId}', 'todo', { input: deleteTodoInput, output: deleteTodoOutput }), controller(deleteTodoController, { useCustomOutput: true }));
 todo.openapi(route.put('/todoId/{todoId}', 'todo', { input: updateTodoInput, output: updateTodoOutput }), controller(updateTodoController));
 
 export default todo;
